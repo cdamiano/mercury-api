ae4acf99fc43dc798908a807cb907693fcfd014e
Con Damianopoulos <con.damianopoulos@rea-group.com>
2017-03-29 19:35:51 +1100

    Add middleware layer

--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
 import express from 'express'
 import consign from 'consign'
-import bodyParser from 'body-parser'
 
-const port = process.env.PORT || 3000;
 const app = express();
-app.set("port", port);
-
-app.use(bodyParser.json());
-
 const cwd = process.env.NODE_ENV === 'production' ? 'dist' : 'src'
+
 consign({cwd: cwd, verbose:true})
+  .then("middleware.js")
   .include("boot.js")
   .then("routes")
   .into(app)
