6ab987e194b984dab108584ae09359ec2f7651d2
Con Damianopoulos <con.damianopoulos@rea-group.com>
2017-03-29 15:50:38 +1100

    Changed directory structure

--- src/index.js
+++ src/index.js
@@ -9,9 +9,10 @@ app.set("port", port);
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-  res.send({"msg": "hello world!!"})
+  res.send({"msg": "hello world"})
 })
 
-consign({verbose:true})
-  .include("lib/boot.js")
+const cwd = process.env.NODE_ENV === 'production' ? 'dist' : 'src'
+consign({cwd: cwd, verbose:true})
+  .include("boot.js")
   .into(app)
