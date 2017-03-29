fd546382161fb0599f9bc272f8b36c1a499604c1
Con Damianopoulos <con.damianopoulos@rea-group.com>
2017-03-30 10:21:01 +1100

    Check if header exists

--- src/forcessl.js
+++ src/forcessl.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-  if(process.env.NODE_ENV === 'production' && req.header('x-forwarded-proto') != 'https') {
+  if(process.env.NODE_ENV === 'production' && req.header('x-forwarded-proto') && req.header('x-forwarded-proto') != 'https') {
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
   } else {
     next();
