1caaf0fda4330eb82839413cf41dc87c71fbdd3e
Con Damianopoulos <con.damianopoulos@rea-group.com>
2017-03-30 14:31:57 +1100

    Add helmet for security

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser'
 import morgan from 'morgan'
 import compression from 'compression'
+import helmet from 'helmet'
 import forcessl from './forcessl.js'
 import logger from "./logger.js"
 
@@ -9,6 +10,7 @@ module.exports = app => {
   app.use(bodyParser.json());
   app.use(forcessl);
   app.use(compression());
+  app.use(helmet());
   app.use(morgan("common", {
     stream: {
       write: (message) => { logger.info(message); }
