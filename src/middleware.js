2edf3e8344d0f949a9183ca3aa9ba8511edfef4c
Con Damianopoulos <con.damianopoulos@rea-group.com>
2017-03-30 14:30:17 +1100

    Add compression

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser'
-import morgan from "morgan"
+import morgan from 'morgan'
+import compression from 'compression'
 import forcessl from './forcessl.js'
 import logger from "./logger.js"
 
@@ -7,6 +8,7 @@ module.exports = app => {
   app.set("port", app.config.port);
   app.use(bodyParser.json());
   app.use(forcessl);
+  app.use(compression());
   app.use(morgan("common", {
     stream: {
       write: (message) => { logger.info(message); }
